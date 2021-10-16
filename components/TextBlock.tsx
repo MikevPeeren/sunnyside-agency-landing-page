caba81973f9022fdfacbc251e975550c6abc07ad
MikevPeeren <mikevpeeren@hotmail.com>
2021-10-16 12:38:44 +0200

    :sparkles: Added barebones text component

--- components/TextBlock.tsx
+++ components/TextBlock.tsx
@@ -1,5 +1,17 @@
-const TextBlock = () => {
-  return <div></div>;
+interface ITextBlock {
+  header: string;
+  content: string;
+  footer: string;
+}
+
+const TextBlock = ({ header, content, footer }: ITextBlock) => {
+  return (
+    <section className="flex flex-col align-center justify-center text-center">
+      <h1 className="p-4 font-bold">{header}</h1>
+      <p>{content}</p>
+      <footer className="p-4 uppercase font-bold">{footer}</footer>
+    </section>
+  );
 };
 
 export default TextBlock;
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,8 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
 
+import TextBlock from "../components/TextBlock";
+
 const Home: NextPage = () => {
   return (
     <div>
@@ -26,6 +28,11 @@ const Home: NextPage = () => {
             width={100}
             height={100}
             layout="responsive"
+          />
+          <TextBlock
+            header="Transform your brand"
+            content="We are a full-service creative agency specializing in helping brands grow fast. Engage your clients through compelling visuals that do most of the marketing for you."
+            footer="Learn More"
           />
         </div>
       </main>
