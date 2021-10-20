6a23f1a75f8ecc826b800f29237c4a264ffa64de
MikevPeeren <mikevpeeren@hotmail.com>
2021-10-20 19:50:09 +0200

    :lipstick: styling for textblock

--- components/TextBlock.tsx
+++ components/TextBlock.tsx
@@ -7,9 +7,11 @@ interface ITextBlock {
 const TextBlock = ({ header, content, footer }: ITextBlock) => {
   return (
     <section className="flex flex-col align-center justify-center text-center my-8">
-      <h1 className="p-4 font-bold">{header}</h1>
-      <p>{content}</p>
-      <footer className="p-4 uppercase font-bold">{footer}</footer>
+      <h1 className="p-4 font-black font-fraunces text-3xl">{header}</h1>
+      <p className="p-4 font-fraunces text-very-dark-gray-blue">{content}</p>
+      <footer className="p-4 uppercase font-black font-fraunces">
+        {footer}
+      </footer>
     </section>
   );
 };
