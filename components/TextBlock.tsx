5ff078b3e4a3512b749e0ec676949aace83f944f
MikevPeeren <mikevpeeren@hotmail.com>
2021-10-21 17:03:14 +0200

    :bug: trying hardcode color

--- components/TextBlock.tsx
+++ components/TextBlock.tsx
@@ -8,15 +8,15 @@ interface ITextBlock {
 }
 
 const TextBlock = ({ header, content, footer, footerColor }: ITextBlock) => {
-  const afterBG = `after:bg-${footerColor}`;
-  const afterBorderBG = `after:border-bg-${footerColor}`;
+  const afterRedBG = `after:bg-yellow`;
+  const afterRedBorderBG = `after:border-bg-soft-red`;
 
   return (
     <section className="flex flex-col align-center justify-center text-center my-8">
       <h1 className="p-4 font-black font-fraunces text-3xl">{header}</h1>
       <p className="p-4 font-fraunces text-very-dark-gray-blue">{content}</p>
       <span
-        className={`${styles.footer} p-4 uppercase font-black font-fraunces ${afterBG} ${afterBorderBG} after:bg-opacity-30`}
+        className={`${styles.footer} p-4 uppercase font-black font-fraunces after:bg-soft-red after:border-bg-soft-red after:bg-opacity-30`}
       >
         {footer}
       </span>
