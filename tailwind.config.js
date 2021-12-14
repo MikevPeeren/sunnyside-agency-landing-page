27a140449c003750a2f0a9e2a5c6829437478e69
MikevPeeren <mikevpeeren@hotmail.com>
2021-12-14 08:38:06 +0100

    :sparkles: updating tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,5 @@
 module.exports = {
-  mode: "jit",
-  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
-  darkMode: false, // or 'media' or 'class'
+  content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
@@ -20,11 +18,6 @@ module.exports = {
         barlow: ["Barlow", "sans-serif"],
         fraunces: ["Fraunces", "serif"],
       },
-    },
-  },
-  variants: {
-    extend: {
-      fill: ["hover", "focus"],
     },
   },
   plugins: [],
